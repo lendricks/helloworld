ead755d17e9c753b9bcd1d8e95cac4335d4ae1a5
lendricks <len.hendricks@gmail.com>
2025-09-09 09:21:04 -0700

    Update streamlit-st.py

--- streamlit-st.py
+++ streamlit-st.py
@@ -3,16 +3,44 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.header('st.write')
+from datetime import time, datetime
+
+st.header('st.slider')
+st.subheader('Slider')
 
 if st.button('Say hello'):
-    st.write('Why hello there')
+    st.write('Hello, *World!* :sunglasses:')
 else:
     st.write('Goodbye')
 
-st.write('Hello, *World!* :sunglasses:')
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, "years old")
 
-st.write(1234)
+st.subheader('Range slider')
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+st.write('Values:', values)
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45))
+)
+st.write("You're scheduled for:", appointment)
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2025, 1, 1, 9, 30),
+    format="MM/DD/YY = hh:mm"
+)
+st.write("Start time:", start_time)
+
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -29,4 +57,5 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
 )
+
 st.write(c)
